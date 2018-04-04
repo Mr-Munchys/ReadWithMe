e0a8a2ff884eb44b3def44d99b6d952e7528ceeb
Camrin Peacock <camrin.peacock@halihealth.com>
2018-04-04 18:13:01 +1000

    Used primary components to create a json book reader. It uses HTML markup in the book to seperate sections/chapters/paragraphs and words into their components.

--- ReadWithMe/App.js
+++ ReadWithMe/App.js
@@ -1,69 +1,134 @@
-import React from 'react';
-import { StyleSheet, Text, View, TouchableHighlight } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity,ToolbarAndroid, Dimensions,Image } from 'react-native';
+import WordItem from './src/components/WordItem/WordItem';
+var bookData = require('./books/json/aliceInWonderland.json');
 
 var  colors = ['#ddd', '#efefef', 'red', '#666', 'rgba(0,0,0,.1)', '#ededed'];
+const window = Dimensions.get('window');
 
-export default class App extends React.Component {
+
+export default class App extends Component {
 
   constructor(props) {
     super(props);
 
-    this.state = {color: '#ddd'};
+    this.state = {
+      bookTitle: 'Book Title1',
+      chapters: "",
+      chapterIds: "",
+      textObjColor: [],
+      textObjWord: []
+    };
 
-    this.color = '#ddd';
-    this.textObjColor = [];
-    this.textObjWord = [];
     this.textObj = [];
-    this.initWords();
-
+    this.loadBook();
   }
 
-  initWords(){
-    for (let i = 0; i < 10; i++) { 
-      var word = "Word" + String(i) + " ";
-        this.textObjWord.push(word);
-
-        this.textObjColor.push('rgb(0,0,0)');
+  initWords = ()=>{
+    var textObjWord = [];
+    var textObjColor = [];
+    for (let i = 0; i < 100; i++) { 
+        var word = "Word" + String(i);
+        textObjWord.push(word);
+        textObjColor.push('rgb(0,0,0)');
       }
+    this.state.textObjColor=textObjColor;
+    this.state.textObjWord=textObjWord;
   }
 
-  _onPressButton(idVal) {
-    console.log('You tapped the button '+this.textObjWord[idVal]+' !');
-    this.textObjColor[idVal] = colors[2];
-    this.color = colors[2];
-    this.setState({color: 'rgb(1,0,0)'});
+   loadBook = ()=>{
+    this.state.bookTitle = bookData["title"];
+    this.state.chapters = bookData["chapters"];
+    this.state.chapterIds = bookData["chapterIds"];
+
+    var section = []
+    var chapterAndHeadings = []
+    chapters = this.state.chapters["item4"].split("<h2>"); //Divide the section into chapters
+
+    var theBook = {};
+    var chapHeader = [], chapBlock = [], chapParagraphs = [];
+    for (let i = 0; i < chapters.length; i++) {
+
+      var chap = chapters[i].split("</h2>"); //for each chapter divide it into header/chapter
+
+      
+      for (let j = 0; j < chap.length; j++) {
+        if(chap[j].length > 200){
+          chapBlock.push(chap[j]);//Save the blocks
+          chapParagraphs.push(chap[j].split("<p>"));//save the chapter split by paragraph
+        }else if(chap[j].length > 10){
+          chapHeader.push(chap[j]); //save the headers
+        }
+      }
+    } 
+
+    this.state.textObjWord = chapParagraphs[0];
+  }
+
+  _onPressButton = (idVal) => {
+    console.log('You tapped the button '+this.state.textObjWord[idVal]+' !');
+    var textObjColor = this.state.textObjColor;
+    textObjColor[idVal] = colors[2];
+    this.setState({textObjColor: textObjColor});
+  }
+
+  _onActionSelected= (pos) => {
+    if (pos === 0) { // index of 'Settings'
+      showSettings();
+    }
   }
 
   render() {
+    
 
     this.textObj = [];
 
-      for (let i = 0; i < this.textObjWord.length; i++) {
-        var word = this.textObjWord[i];
+      for (let i = 0; i < this.state.textObjWord.length; i++) {
         this.textObj.push(  
-                <Text key={i} style={{color: this.textObjColor[i] }} onPress={ () => this._onPressButton(i) } >{word}</Text>    
+                <Text key={i} style={{color: this.state.textObjColor[i] }} onPress={ () => this._onPressButton(i) } >{this.state.textObjWord[i]}</Text>    
           )
       }
 
+      const words = this.state.textObjWord.map((word,i) => (
+        <WordItem key={i} color={this.state.textObjColor[i]} wordText={this.state.textObjWord[i] } postSymbol=" " preSymbol="" />
+      ));
+
     return (
+      <View style={styles.mainContainer}>
+        <View style={styles.toolbar}>
+          <TouchableOpacity onPress={this.loadBook}><Image style={styles.iconIm} source={require('./icon_library.png')} /></TouchableOpacity>
+          <Text style={styles.toolbarTitle}>{this.state.bookTitle}</Text>
+          <TouchableOpacity onPress={this._onPressSettings}><Image style={styles.iconIm} source={require('./icon_settings.png')} /></TouchableOpacity>
+        </View>
 
+        <View style={styles.content}>
+          {words}
+        </View>
 
-      <View style={styles.container}>
-        <Text>{this.textObj}</Text>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  mainContainer:{
+    flex:1                  //Step 1
   },
+
+  content:{
+    backgroundColor:'#fff',
+    flexDirection: 'row',
+    flex: 1,
+    flexWrap: 'wrap',
+    alignItems: "flex-start",
+    justifyContent: "flex-start",
+    padding: 0,
+    margin: 0,
+  },
+
   baseText: {
     fontFamily: 'Cochin',
+    fontSize: 15
   },
   correctText: {
     fontFamily: 'Cochin',
@@ -77,4 +142,26 @@ const styles = StyleSheet.create({
     fontFamily: 'Cochin',
     color: 'blue',
   },
+  toolbar:{
+    backgroundColor:'#1abc9c',
+    paddingTop:10,
+    paddingBottom:10,
+    flexDirection:'row'    //Step 1
+},
+toolbarButton:{
+    width: 50,            //Step 2
+    color:'#fff',
+    textAlign:'center'
+},
+toolbarTitle:{
+    color:'#fff',
+    textAlign:'center',
+    fontWeight:'bold',
+    flex:1,                //Step 3
+    fontSize: 15
+},
+iconIm: {
+  width: 28,
+  height: 28
+}
 });
